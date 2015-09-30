270ad6c9ed59fdad140f88ef616da53b73457fb4
Ayman Khalil <aymkhalil85@gmail.com>
2015-09-30 16:20:05 +0300

    Redirect cython binaries to the current directory

--- src/pyclaw/classic/setup_cy.py
+++ src/pyclaw/classic/setup_cy.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
+extensions=[Extension("solver_cy",["solver_cy.pyx"])]
+
 setup(
-    ext_modules = cythonize("solver_cy.pyx"),
+    ext_modules = cythonize(extensions),
     include_dirs=[numpy.get_include()]
 )
--- src/pyclaw/limiters/setup_cy.py
+++ src/pyclaw/limiters/setup_cy.py
@@ -1,8 +1,11 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 
+extensions=[Extension("tvd_cy",["tvd_cy.pyx"])]
+
 setup(
-    ext_modules = cythonize("tvd_cy.pyx"),
+    ext_modules = cythonize(extensions),
     include_dirs=[numpy.get_include()]
 )
